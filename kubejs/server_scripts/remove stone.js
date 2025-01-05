cbf4c98d5fca64626e6989fafcb5606bd5bcd03a
XDSenDX <xeseninanutshell@gmail.com>
2025-01-05 19:34:57 +0700

    1/5/2025 build
    okay

---
+++ kubejs/server_scripts/remove stone.js
@@ -0,0 +1,28 @@
+ServerEvents.recipes((e) => {
+ 
+  e.remove({ id: "magistuarmory:stone_stylet" }) /// remove stylet
+  e.remove({ id: "magistuarmory:stone_shortsword" }) /// remove short sword
+  e.remove({ id: "magistuarmory:stone_katzbalger" }) /// remove katzbalger
+  e.remove({ id: "magistuarmory:stone_pike" }) /// remove pike
+  e.remove({ id: "magistuarmory:stone_ranseur" }) /// remove  ranseur
+  e.remove({ id: "magistuarmory:stone_ahlspiess" }) /// remove ahlspiess 
+  e.remove({ id: 'magistuarmory:stone_chivalrylance' }) /// remove  lance
+  e.remove({ id: "magistuarmory:stone_bastardsword" }) /// remove  bastard 
+  e.remove({ id: "magistuarmory:stone_estoc" }) /// remove  estoc 
+  e.remove({ id: "magistuarmory:stone_claymore" }) /// remove  claymore 
+  e.remove({ id: "magistuarmory:stone_zweihander" }) /// removezweihander 
+  e.remove({ id: "magistuarmory:stone_flamebladedsword" }) /// remove flamebladed 
+  e.remove({ id: "magistuarmory:stone_lochaberaxe" }) /// remove  lochaberaxe 
+  e.remove({ id: "magistuarmory:stone_concavehalberd" }) /// remove  concavehalberd 
+  e.remove({ id: "magistuarmory:stone_heavymace" }) /// remove  heavy mace 
+  e.remove({ id: "magistuarmory:stone_heavywarhammer" }) /// remove  heavy war hammer 
+  e.remove({ id: "magistuarmory:stone_lucernhammer" }) /// remove  lucerne hammer 
+  e.remove({ id: "magistuarmory:stone_morgenstern" }) /// remove morning star 
+  e.remove({ id: "magistuarmory:stone_chainmorgenstern" }) /// remove flail 
+  e.remove({ id: "magistuarmory:stone_guisarme" }) /// remove  guisarme 
+ 
+  e.remove({ id: "epicfight:stone_greatsword" }) /// remove greatsword
+  e.remove({ id: "epicfight:stone_spear" }) /// remove  spear
+
+});
+
---
+++ kubejs/server_scripts/remove wooden.js
@@ -0,0 +1,28 @@
+ServerEvents.recipes((e) => {
+  e.remove({ id: "epic_knights__japanese_armory:wnaginata" }) /// remove naginata
+  e.remove({ id: "epic_knights__japanese_armory:wkatana" }) /// remove katana
+  e.remove({ id: "magistuarmory:wood_stylet" }) /// remove stylet
+  e.remove({ id: "magistuarmory:wood_shortsword" }) /// remove short sword
+  e.remove({ id: "magistuarmory:wood_katzbalger" }) /// remove katzbalger
+  e.remove({ id: "magistuarmory:wood_pike" }) /// remove pike
+  e.remove({ id: "magistuarmory:wood_ranseur" }) /// remove  ranseur
+  e.remove({ id: "magistuarmory:wood_ahlspiess" }) /// remove ahlspiess 
+  e.remove({ id: 'magistuarmory:wood_chivalrylance' }) /// remove  lance
+  e.remove({ id: "magistuarmory:wood_bastardsword" }) /// remove  bastard 
+  e.remove({ id: "magistuarmory:wood_estoc" }) /// remove  estoc 
+  e.remove({ id: "magistuarmory:wood_claymore" }) /// remove  claymore 
+  e.remove({ id: "magistuarmory:wood_zweihander" }) /// removezweihander 
+  e.remove({ id: "magistuarmory:wood_flamebladedsword" }) /// remove flamebladed 
+  e.remove({ id: "magistuarmory:wood_lochaberaxe" }) /// remove  lochaberaxe 
+  e.remove({ id: "magistuarmory:wood_concavehalberd" }) /// remove  concavehalberd 
+  e.remove({ id: "magistuarmory:wood_heavymace" }) /// remove  heavy mace 
+  e.remove({ id: "magistuarmory:wood_heavywarhammer" }) /// remove  heavy war hammer 
+  e.remove({ id: "magistuarmory:wood_lucernhammer" }) /// remove  lucerne hammer 
+  e.remove({ id: "magistuarmory:wood_morgenstern" }) /// remove morning star 
+  e.remove({ id: "magistuarmory:wood_chainmorgenstern" }) /// remove flail 
+  e.remove({ id: "magistuarmory:wood_guisarme" }) /// remove  guisarme 
+ 
+
+
+});
+
